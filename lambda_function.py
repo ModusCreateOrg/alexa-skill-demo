d2caa0391132e8ccc70c884a831c54a22aeb2945
Simon Prickett <simon@Simons-MacBook-Pro.local>
2016-07-06 10:10:34 -0700

    Standardized use of quotation marks.

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,19 @@ import json
 API_BASE="http://bartjsonapi.elasticbeanstalk.com/api"
 
 def lambda_handler(event, context):
-    if (event['session']['application']['applicationId'] !=
-            "amzn1.echo-sdk-ams.app.XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
+    if (event["session"]["application"]["applicationId"] !=
+            "amzn1.echo-sdk-ams.app.bd304b90-xxxx-xxxx-xxxx-xxxxd4772bab"):
         raise ValueError("Invalid Application ID")
     
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']}, event['session'])
+    if event["session"]["new"]:
+        on_session_started({"requestId": event["request"]["requestId"]}, event["session"])
 
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+    if event["request"]["type"] == "LaunchRequest":
+        return on_launch(event["request"], event["session"])
+    elif event["request"]["type"] == "IntentRequest":
+        return on_intent(event["request"], event["session"])
+    elif event["request"]["type"] == "SessionEndedRequest":
+        return on_session_ended(event["request"], event["session"])
 
 def on_session_started(session_started_request, session):
     print "Starting new session."
@@ -25,8 +25,8 @@ def on_launch(launch_request, session):
     return get_welcome_response()
 
 def on_intent(intent_request, session):
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
+    intent = intent_request["intent"]
+    intent_name = intent_request["intent"]["name"]
 
     if intent_name == "GetStatus":
         return get_system_status()
@@ -111,7 +111,7 @@ def get_train_times(intent):
 
             speech_output = "Train departures from " + station_name + " are as follows: "
             for destination in station_departures["etd"]:
-                speech_output += 'Towards ' + destination["destination"] + " on platform " + destination["estimate"][0]["platform"] + '. ';
+                speech_output += "Towards " + destination["destination"] + " on platform " + destination["estimate"][0]["platform"] + ". ";
                 for estimate in destination["estimate"]:
                     if estimate["minutes"] == "Leaving":
                         speech_output += "Leaving now: "
@@ -179,27 +179,27 @@ def get_station_code(station_name):
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
+        "outputSpeech": {
+            "type": "PlainText",
+            "text": output
         },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
+        "card": {
+            "type": "Simple",
+            "title": title,
+            "content": output
         },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
+        "reprompt": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": reprompt_text
             }
         },
-        'shouldEndSession': should_end_session
+        "shouldEndSession": should_end_session
     }
 
 def build_response(session_attributes, speechlet_response):
     return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
+        "version": "1.0",
+        "sessionAttributes": session_attributes,
+        "response": speechlet_response
     }
